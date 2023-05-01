ac42df62ca0f373bdc0fd5c122d05e2892238c46
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2023-05-01 10:21:21 -0300

    Add execCommand

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,32 @@
 #!/usr/bin/env node
 
-const x: number = 123
-console.log("hi this is a test", x)
+import { exec as _exec } from "child_process"
+import { promisify } from "util"
 
+// command to run
+const testCmd: Command = {
+  title: "test",
+  command: "sleep 2 && printf 'hi mom'"
+}
+
+// execute command
+const exec = promisify(_exec)
+
+interface Command {
+  title: string
+  command: string
+}
+
+// test exec
+// exec(command)
+// .then(({ stdout }) => {
+//   console.log(`out: ${stdout}`)
+// })
+
+const runOneCommand = (cmd: Command) => exec(cmd.command).then(({ stdout }) => {
+  console.table([{ title: cmd.title, out: stdout }])
+  // console.log(`Result: ${stdout}`)
+})
+
+
+runOneCommand(testCmd)
